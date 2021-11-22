5f07d5e04c5224787cc8e7618ffb29b9b46c324e
youngminkun <rhans123@naver.com>
2021-11-22 18:35:34 +0900

    basic

---
+++ if_test.js
@@ -0,0 +1,12 @@
+if (true && true) {
+  console.log("둘다 true 이므로 출력됩니다.");
+}
+if (true && false) {
+  console.log("둘중 하나가 false 이므로 출력되지 않습니다.");
+}
+if (true || false) {
+  console.log("둘중 하나라도 true 이므로 출력됩니다.");
+}
+if (false || false) {
+  console.log("둘다 flase 이므로 출력도지 않습니다.");
+}
