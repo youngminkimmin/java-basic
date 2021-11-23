dc72e12b716e72ff3be828e2dd2464d7276c68b2
youngmin <rhans123@naver.com>
2021-11-23 17:17:46 +0900

    banbok

---
+++ loop.js
@@ -0,0 +1,11 @@
+//console.log("hello js");
+/*let i = 1;
+let num = i++;
+
+console.log(i, "===", num);*/
+let sum = 0;
+for (let i = 2; i <= 100; i += 2) {
+  sum = sum + i;
+  //sum+=il
+}
+console.log(sum);
